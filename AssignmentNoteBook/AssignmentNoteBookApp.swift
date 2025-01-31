062947b85b6f38d8e0e0fcde61b6e61299ce0016
Zane <zmatarieh@bsd220.org>
2025-01-31 09:46:27 -0600

    Initial Commit

---
+++ AssignmentNoteBook/AssignmentNoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNoteBookApp.swift
+//  AssignmentNoteBook
+//
+//  Created by Zane Matarieh on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentNoteBookTests/AssignmentNoteBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNoteBookTests.swift
+//  AssignmentNoteBookTests
+//
+//  Created by Zane Matarieh on 1/31/25.
+//
+
+import Testing
+@testable import AssignmentNoteBook
+
+struct AssignmentNoteBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
